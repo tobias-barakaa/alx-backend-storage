3d3fca191e4acdd0e5339df4a3085b03e04c23b8
root <ngairato@gmail.com>
2023-12-15 22:59:55 +0000

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,16 +1,5 @@
--- create temporary table
-
-DELIMITER //
-
-CREATE TEMPORARY TABLE band_counts (
-  origin VARCHAR(255) PRIMARY KEY,
-  fan_count INT NOT NULL DEFAULT 0
-);
-
-INSERT INTO band_counts (origin, fan_count)
-SELECT origin, SUM(nb_fans) AS total_fans
+-- create origin
+SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
-GROUP BY origin;
-//
-
-DELIMITER ;
+GROUP BY origin
+ORDER BY nb_fans DESC;
