23535eac30ef060c5a295e0cd3bd1a87fa61356c
Baraka Ngaira <ngairato@gmail.com>
2023-12-16 16:51:32 +0300

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
--- Ranks countries by number of non-unique fans
-SELECT origin, SUM(fans) AS nb_fans
+-- SQL script that lists all rock as their main style
+SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
