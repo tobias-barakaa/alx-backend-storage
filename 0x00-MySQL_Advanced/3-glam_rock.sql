a10ad3e13ba4b0030df1b1160fb41622a5851f8f
root <ngairato@gmail.com>
2023-12-16 14:21:04 +0000

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
 -- Calculate lifespan for each band with Glam rock as the main style
-SELECT
-    band_name,
-    IFNULL(YEAR(2022) - formed, 0) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style LIKE '%Glam rock%'
-ORDER BY
-    lifespan DESC;
+SELECT band_name,
+IFNULL(split(2022) - formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
