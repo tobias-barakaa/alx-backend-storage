1ac84171d29a2675e4b5fd78c3f03461966f19e1
root <ngairato@gmail.com>
2023-12-16 15:15:48 +0000

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,6 @@
--- Calculate lifespan for each band with Glam rock as the main style
-SELECT
-    band_name,
-    IFNULL(
-        CASE
-            WHEN split IS NOT NULL THEN split - formed
-            ELSE YEAR(2022) - formed
-        END,
-        0
-    ) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style LIKE '%Glam rock%'
-ORDER BY
-    lifespan DESC;
+-- SQL script that lists all bands with Glam rock
+SELECT band_name, 
+IFNULL(split, 2022) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC, band_name;
