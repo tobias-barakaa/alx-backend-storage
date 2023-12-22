be895003a8a33aed744395bc46b396daa17d6029
tobias-barakaa <ngairato@gmail.com>
2023-12-22 13:18:18 -0800

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Main file
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -34,3 +34,34 @@ class Cache:
 
         # Return the generated key
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
+        """
+        Retrieve data from Redis using the provided key and optionally apply a conversion function.
+
+        :param key: The key used to retrieve data from Redis.
+        :param fn: Optional conversion function to apply on the retrieved data.
+        :return: The retrieved data, optionally converted using the provided function.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieve a string from Redis using the provided key.
+
+        :param key: The key used to retrieve data from Redis.
+        :return: The retrieved string or None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieve an integer from Redis using the provided key.
+
+        :param key: The key used to retrieve data from Redis.
+        :return: The retrieved integer or None if the key does not exist.
+        """
+        return self.get(key, fn=int)
