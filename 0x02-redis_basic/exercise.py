f32aed36124feb44f105636427a9168c23e6d8cc
tobias-barakaa <ngairato@gmail.com>
2023-12-22 14:35:35 -0800

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,8 +4,7 @@ Main file
 """
 import redis
 import uuid
-from typing import Union
-
+from typing import Callable, Optional, Union
 
 class Cache:
     def __init__(self):
@@ -13,12 +12,37 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store the input data in Redis using a random key.
-
-        :param data: The data to be stored.
-        :return: The key used to store the data.
-        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Get the value of the key and apply the conversion function if it exists.
+
+        :param key: The key to get the value of.
+        :param fn: The function to convert the value with.
+        :return: The value of the key.
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Get the value of the key and convert it to a string.
+
+        :param key: The key to get the value of.
+        :return: The value of the key, converted to a string.
+        """
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Get the value of the key and convert it to an integer.
+
+        :param key: The key to get the value of.
+        :return: The value of the key, converted to an integer.
+        """
+        return self.get(key, fn=int)
