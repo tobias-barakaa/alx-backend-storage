f1f2ed677039b310dfe9652e62a8569ffc27f0e5
tobias-barakaa <ngairato@gmail.com>
2023-12-22 14:32:42 -0800

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,84 +4,21 @@ Main file
 """
 import redis
 import uuid
-from typing import Union, Callable
-from functools import wraps
+from typing import Union
+
 
 class Cache:
     def __init__(self):
-        """
-        Initialize the Cache class with a Redis client instance and flush the database.
-        """
-        # Create an instance of the Redis client and flush the database
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        """
-        Decorator to count the number of calls to a method.
-
-        :param method: The method to be decorated.
-        :return: Decorated method.
-        """
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            key = method.__qualname__
-            # Increment the count for the method key
-            count_key = f"{key}_calls_count"
-            self._redis.incr(count_key)
-            # Call the original method and return its result
-            result = method(self, *args, **kwargs)
-            return result
-        return wrapper
-
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Store the input data in Redis using a randomly generated key.
+        Store the input data in Redis using a random key.
 
-        :param data: The data to be stored, can be str, bytes, int, or float.
-        :return: The randomly generated key used for storing the data.
+        :param data: The data to be stored.
+        :return: The key used to store the data.
         """
-        # Generate a random key using uuid
         key = str(uuid.uuid4())
-        
-        # Store the input data in Redis using the random key
-        if isinstance(data, (str, bytes, int, float)):
-            self._redis.set(key, data)
-        else:
-            raise ValueError("Invalid data type. Supported types are str, bytes, int, or float.")
-
-        # Return the generated key
+        self._redis.set(key, data)
         return key
-
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
-        """
-        Retrieve data from Redis using the provided key and optionally apply a conversion function.
-
-        :param key: The key used to retrieve data from Redis.
-        :param fn: Optional conversion function to apply on the retrieved data.
-        :return: The retrieved data, optionally converted using the provided function.
-        """
-        data = self._redis.get(key)
-        if data is not None and fn is not None:
-            return fn(data)
-        return data
-
-    def get_str(self, key: str) -> Union[str, None]:
-        """
-        Retrieve a string from Redis using the provided key.
-
-        :param key: The key used to retrieve data from Redis.
-        :return: The retrieved string or None if the key does not exist.
-        """
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        """
-        Retrieve an integer from Redis using the provided key.
-
-        :param key: The key used to retrieve data from Redis.
-        :return: The retrieved integer or None if the key does not exist.
-        """
-        return self.get(key, fn=int)
